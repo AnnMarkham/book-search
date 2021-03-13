0d13870a87f3ab3f0e4667379ff92b119c53c1b1
AnnMarkham <ann.e.markham@outlook.com>
2021-03-13 12:21:57 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
 const db = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const { ApolloServer } = require("apollo-server-express");
+
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const server = new ApolloServer({
